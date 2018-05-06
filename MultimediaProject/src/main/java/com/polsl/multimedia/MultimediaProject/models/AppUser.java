ea60ee4d3834510b0d21f450fa98909e63f4a72d
lothar90 <rafalswoboda@wp.pl>
2018-05-06 18:32:08 +0200

    Changed database for Ligus purposes. But it doesn't really work.

--- MultimediaProject/src/main/java/com/polsl/multimedia/MultimediaProject/models/AppUser.java
+++ MultimediaProject/src/main/java/com/polsl/multimedia/MultimediaProject/models/AppUser.java
@@ -1,7 +1,11 @@
 package com.polsl.multimedia.MultimediaProject.models;
 
 import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -9,28 +13,44 @@ import java.util.List;
  */
 
 @Entity
+@NamedQueries({
+        @NamedQuery(name = "AppUser.findAll", query = "SELECT a FROM AppUser a")
+        , @NamedQuery(name = "AppUser.findById", query = "SELECT a FROM AppUser a WHERE a.id = :id")
+        , @NamedQuery(name = "AppUser.findByPassword", query = "SELECT a FROM AppUser a WHERE a.password = :password")
+        , @NamedQuery(name = "AppUser.findByUsername", query = "SELECT a FROM AppUser a WHERE a.username = :username")})
+public class AppUser implements Serializable {
 
-public class AppUser {
+    private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue
-    @Column(name = "ID", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Basic(optional = false)
+    @Column(name = "id")
     private Long id;
-
-    @Column(name = "username", nullable = false)
-    private String username;
-
-    @Column(name = "password", nullable = false)
+    @Basic(optional = false)
+    @Column(name = "password")
     private String password;
-
-    @OneToMany(cascade = CascadeType.ALL)
-    private List<Photo> photos = new ArrayList<>();
+    @Basic(optional = false)
+    @Column(name = "username")
+    private String username;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "userID")
+    private List<Photo> photoCollection;
 
     public AppUser() {
     }
 
-    public AppUser(String username, String password) {
-        this.username = username;
+    public AppUser(Long id) {
+        this.id = id;
+    }
+
+    public AppUser(Long id, String password, String username) {
+        this.id = id;
         this.password = password;
+        this.username = username;
+    }
+
+    public AppUser(String password, String username) {
+        this.password = password;
+        this.username = username;
     }
 
     public Long getId() {
@@ -41,14 +61,6 @@ public class AppUser {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getPassword() {
         return password;
     }
@@ -57,11 +69,46 @@ public class AppUser {
         this.password = password;
     }
 
-    public List<Photo> getPhotos() {
-        return photos;
+    public String getUsername() {
+        return username;
     }
 
-    public void setPhotos(List<Photo> photos) {
-        this.photos = photos;
+    public void setUsername(String username) {
+        this.username = username;
     }
+
+    @XmlTransient
+    public List<Photo> getPhotos() {
+        return photoCollection;
+    }
+
+    public void setPhotos(List<Photo> photoCollection) {
+        this.photoCollection = photoCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof AppUser)) {
+            return false;
+        }
+        AppUser other = (AppUser) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "javaapplication1.AppUser[ id=" + id + " ]";
+    }
+
 }
