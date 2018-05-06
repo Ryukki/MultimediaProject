2e08d15f0d19e0464b228bdb5f1c82aa24f26e1e
lothar90 <rafalswoboda@wp.pl>
2018-05-06 18:08:22 +0200

    Added some dependencies, so now *.war works.

--- MultimediaProject/src/main/java/com/polsl/multimedia/MultimediaProject/MultimediaProjectApplication.java
+++ MultimediaProject/src/main/java/com/polsl/multimedia/MultimediaProject/MultimediaProjectApplication.java
@@ -2,11 +2,22 @@ package com.polsl.multimedia.MultimediaProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MultimediaProjectApplication {
+public class MultimediaProjectApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+//	public static void main(String[] args) {
+//		SpringApplication.run(MultimediaProjectApplication.class, args);
+//	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MultimediaProjectApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(MultimediaProjectApplication.class, args);
 	}
 }
